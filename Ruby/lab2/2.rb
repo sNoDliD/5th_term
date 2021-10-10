00dba09db0f7240efaa9a5e1970ca6e25b11593a
Dima <dmitro-sv@outlook.com>
2021-10-10 15:44:38 +0300

    ruby 2

---
+++ Ruby/lab2/2.rb
@@ -0,0 +1,7 @@
+require 'bigdecimal'
+u = 4/5.to_r #=>
+r = 8.3.to_r #=>
+s = Rational("2.4") #=>
+t = BigDecimal "56/78", 10 #=>
+w = BigDecimal "565.89",10 #=>
+puts u; puts s ; puts t ; puts r; puts w
